ffc537c6826201666dc7eb8fba97899de17b9a78
Sebastian Czech <sebaczech@gmail.com>
2020-02-20 16:55:50 +0100

    Code initialized from https://start.spring.io

---
+++ src/test/java/com/sebastianczech/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastianczech.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
