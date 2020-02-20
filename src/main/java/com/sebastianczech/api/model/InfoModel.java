d8163fbb007e03a8f0f532d035317b71fe2c2629
Sebastian Czech <sebaczech@gmail.com>
2020-02-20 17:14:54 +0100

    model: Simple model with ID and NAME

---
+++ src/main/java/com/sebastianczech/api/model/InfoModel.java
@@ -0,0 +1,17 @@
+package com.sebastianczech.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class InfoModel {
+
+    private Integer id;
+    private String name;
+
+}
